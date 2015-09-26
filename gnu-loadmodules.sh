acec4a316cbc99ec6cbd48283c2bfac71ce90892
Patrick Sanan <patrick.sanan@gmail.com>
2015-09-26 14:48:28 +0200

    simplify gnu modules

--- gnu-loadmodules.sh
+++ gnu-loadmodules.sh
@@ -2,7 +2,6 @@ module unload PrgEnv-cray
 module unload PrgEnv-pgi
 module load PrgEnv-gnu
 module unload cray-mpich
-module load cray-mpich/7.2.0
 module load cudatoolkit
 module load cmake
 module load python/2.7.6
