9ef782ebdfd728ff4ecb1dae7e5b823f9ce46ff9
Patrick Sanan <patrick.sanan@gmail.com>
2016-05-11 20:20:57 +0200

    Add intel config for Juraj

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 To get PETSc running on daint
 
-1) Load up the modules you want. For cray, it might be okay to use the defaults (PrgEnv-cray and cray-mpich). You may also want cudatoolkit.
+1) Load up the modules you want. For cray, it might be okay to use the defaults (PrgEnv-cray and cray-mpich). You may also want cudatoolkit or cmake.
 
 2) Configure and build PETSc. For example (with gnu, for cray or a different arch, use the correspondingly-named files)
     git clone https://bitbucket.org/petsc/petsc
@@ -10,16 +10,3 @@ To get PETSc running on daint
     python ../CSCS_petsc_helpers/arch-gnu-xc30-daint.py
     make PETSC_ARCH=arch-gnu-xc30-daint PETSC_DIR=$PWD
     . ../CSCS_petsc_helpers/arch-gnu-xc30-daint-test.sh
-
-----------------------------------------------------------
-OLD:
- 3) To test GPU subsolves, you can do something like
-    [set up a remote, check out psanan/pc-asm-sub-type]
-    make
-    export PETSC_DIR=$PWD
-    export PETSC_ARCH=arch-gnu-xc30-daint
-    cd src/ksp/ksp/examples/tutorials
-    make ex23
-    sbatch ~/CSCS_petsc_helpers/ex23_twogpus.sbatch
-    watch squeue -u psanan
-    cat ex23_twogpus.out
---
+++ arch-intel-xc30-daint.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+# Do the following before running this configure script [daint.cscs.ch]
+#
+#   module unload PrgEnv-cray
+#   module load PrgEnv-intel
+#   module load cmake
+
+import os
+
+MKLROOT=os.getenv('MKLROOT')
+if not MKLROOT :
+  raise Exception("MKLROOT not defined in the environment. Did you forget to load the PrgEnv-intel module?")
+
+
+configure_options = [
+# On cray cc,CC,ftn are equivalent to mpicc,mpiCC,mpif90
+  '--with-cc=cc',
+  '--with-cxx=0',
+  '--with-fc=ftn',
+
+  '--COPTFLAGS=',
+  '--CXXOPTFLAGS=',
+  '--FOPTFLAGS=',
+
+  '--with-clib-autodetect=0',
+  '--with-cxxlib-autodetect=0',
+  '--with-fortranlib-autodetect=0',
+
+  '--with-shared-libraries=0',
+  '--with-debugging=0',
+  '--with-valgrind=0',
+
+  '--known-mpi-shared-libraries=1',
+
+  '--with-x=0',
+
+  # From Intel MKL Link Advisor
+  #  -L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lpthread -lm -ldl
+  # and note the trailing -mkl which seems to be required to avoid a undefined symbol when linking
+  '--with-blas-lapack-lib=[-L'+MKLROOT+'/lib/intel64,-lmkl_intel_lp64,-lmkl_core,-mkl]',
+  '--with-mkl_pardiso=1',
+  '--with-mkl_pardiso-dir='+MKLROOT,
+
+
+  'PETSC_ARCH=arch-intel-xc30-daint',
+
+  ]
+
+if __name__ == '__main__':
+  import sys,os
+  sys.path.insert(0,os.path.abspath('config'))
+  import configure
+  configure.petsc_configure(configure_options)
