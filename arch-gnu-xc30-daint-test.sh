80f2833c854e9cd350e1cd44c8802f6867e42f5c
Patrick Sanan <patrick.sanan@gmail.com>
2016-06-06 16:48:12 +0200

    Update aprun-->srun

--- arch-gnu-xc30-daint-test.sh
+++ arch-gnu-xc30-daint-test.sh
@@ -15,7 +15,7 @@ echo '#SBATCH --ntasks=1' >> petsc_ex19_test1.batch
 echo '#SBATCH --time=00:00:03' >> petsc_ex19_test1.batch
 echo '#SBATCH --account=c05' >> petsc_ex19_test1.batch
 echo '#SBATCH --output=arch-gnu-xc30-daint-test1.out' >> petsc_ex19_test1.batch
-echo 'aprun -n 1 ./src/snes/examples/tutorials/ex19 -da_refine 3 -snes_monitor_short -pc_type mg -ksp_type fgmres -pc_mg_type full' >> petsc_ex19_test1.batch
+echo 'srun -n 1 ./src/snes/examples/tutorials/ex19 -da_refine 3 -snes_monitor_short -pc_type mg -ksp_type fgmres -pc_mg_type full' >> petsc_ex19_test1.batch
 sbatch petsc_ex19_test1.batch
 rm petsc_ex19_test1.batch
 
@@ -28,7 +28,7 @@ rm petsc_ex19_test1.batch
  echo '#SBATCH --time=00:00:03' >> petsc_ex19_test2.batch
 echo '#SBATCH --account=c05' >> petsc_ex19_test2.batch
  echo '#SBATCH --output=arch-gnu-xc30-daint-test2.out' >> petsc_ex19_test2.batch
- echo 'aprun -n 2 ./src/snes/examples/tutorials/ex19 -da_refine 3 -snes_monitor_short -pc_type mg -ksp_type fgmres -pc_mg_type full' >> petsc_ex19_test2.batch
+ echo 'srun -n 2 ./src/snes/examples/tutorials/ex19 -da_refine 3 -snes_monitor_short -pc_type mg -ksp_type fgmres -pc_mg_type full' >> petsc_ex19_test2.batch
  sbatch petsc_ex19_test2.batch
  rm -f petsc_ex19_test2.batch
 
