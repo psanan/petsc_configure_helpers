dc16be429973f32739f89d467e6a00b0ac79a37a
Patrick Sanan <patrick.sanan@gmail.com>
2017-09-29 15:40:00 +0200

    Update for latest pythontestharness, daint xc30-->xc50

--- arch-gnu-xc50-daint.py
+++ arch-gnu-xc50-daint.py
@@ -8,15 +8,12 @@ import os
 #
 #  module unload PrgEnv-cray && module load PrgEnv-gnu && module load cudatoolkit
 
-
 # Get CUDATOOLKIT_HOME from environment
 #CUDATOOLKIT_HOME=os.getenv('CUDATOOLKIT_HOME')
 #if not CUDATOOLKIT_HOME :
 #  raise Exception("CUDATOOLKIT_HOME not defined in the environment. Did you forget to load modules?")
 
 configure_options = [
-# On cray cc,CC,ftn are eqivalent to mpicc,mpiCC,mpif90
-# Note that we add some flags, OVERRIDING any existing COPTFLAGS ans CXXOPTFLAGS..
   '--with-cc=cc',
   '--with-cxx=CC',
   '--with-fc=ftn',
@@ -45,19 +42,16 @@ configure_options = [
   '--with-x=0',
   #'--with-hwloc=0'
 
-  'PETSC_ARCH=arch-gnu-xc30-daint',
-  #'--with-blas-lapack-lib=-L/opt/cray...'
+  'PETSC_ARCH=arch-gnu-xc50-daint',
 
   #'--with-opencl',
   #'--with-opencl-lib='+CUDATOOLKIT_HOME+'/lib64/libOpenCL.so',
   #'--with-opencl-include='+CUDATOOLKIT_HOME+'/include',
 
   #'--download-viennacl',
-
   #'--with-viennacl=1',
   #'--with-viennacl-include=../viennacl-dev',
   #'--with-viennacl-lib= ',
-
   ]
 
 if __name__ == '__main__':
