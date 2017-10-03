b615d5dab39c5f7f82e49a0b05e93eed85ceb9ba
Patrick Sanan <patrick.sanan@gmail.com>
2017-10-03 08:21:35 +0200

    petsc_configure_common.sh: adjust EXTRA packages for single precision, and other cleanup

--- petsc_configure_common.sh
+++ petsc_configure_common.sh
@@ -8,7 +8,7 @@
 # for a version with debugging and extra packages
 #
 #     EXTRA=1 ARCHMOD=maint /path/to/here/petsc_configure_common.sh
-# 
+#
 # You can also provide things like PRECISION, SCALARTYPE, etc.
 # You can directly pass any other flags you'd like with the EXTRAFLAGS variable
 #
@@ -37,18 +37,15 @@ if [ "$ARCHMOD" != "" ]; then
   PETSC_ARCH+=-$ARCHMOD
 fi
 
-# Set Precision (double or single), and an appropriate BLAS/LAPACK 
+# Set Precision
 PRECISION=${PRECISION:-double}
 if [ "$PRECISION" == "double" ]; then
     PETSC_ARCH+=-double
-    BLAS_LAPACK=" --download-fblaslapack "
 elif [ "$PRECISION" == "single" ]; then
     PETSC_ARCH+=-single
-    BLAS_LAPACK=" --download-fblaslapack "
 elif [ "$PRECISION" == "__float128" ]; then
     PETSC_ARCH+=-float128
-    BLAS_LAPACK=" --download-f2cblaslapack "
-else 
+else
   echo "unrecognized precision $PRECISION provided (try python2 ./configure help | grep precision to see available options)"
   exit 1
 fi
@@ -59,7 +56,7 @@ USE_VIENNACL=${USE_VIENNACL:-0}
 # Set to 0 to skip using c2html
 USE_C2HTML=${USE_C2HTML:-1}
 
-# Set to 1 to just download Suitesparse (on by default, since it's handy
+# Set to 1 to just download SuiteSparse (on by default, since it's handy
 #  to have a sparse direct solver available)
 USE_SUITESPARSE=${USE_SUITESPARSE:-1}
 
@@ -67,13 +64,11 @@ USE_SUITESPARSE=${USE_SUITESPARSE:-1}
 # Set to 1 to use ../viennacl-dev
 VIENNACL_DEV=${VIENNACL_DEV:-0}
 
-# Set to 1 to add a common set of external solvers
+# Set to 1 to add a common set of external packages
+# Some are only available for certain values of PRECISION
 EXTRA=${EXTRA:-0}
-if [ "$EXTRA" == 1 ]; then
-  EXTRAOPTS=" --download-suitesparse --download-hdf5 --download-sundials --download-scalapack --download-metis --download-parmetis --download-ptscotch --download-mumps --download-sundials --download-triangle --download-exodusii --download-netcdf --download-chaco" 
+if [ "$EXTRA" == "1" ]; then
   PETSC_ARCH+=-extra
-else
-  EXTRAOPTS=""
 fi
 
 # Set scalar type (real or complex)
@@ -82,27 +77,21 @@ if [ "$SCALARTYPE" == "real" ]; then
     PETSC_ARCH+=
 elif [ "$SCALARTYPE" == "complex" ]; then
     PETSC_ARCH+=-complex
-    if [ $USE_VIENNACL == 1 ]; then
-      echo "Warning: cannot use ViennaCL with complex scalars. Deactivating ViennaCL"
-      USE_VIENNACL=0
-    fi
-else 
+else
   echo "unrecognized scalar type $SCALARTYPE provided "
   exit 1
 fi
 
 # Set Debug status (1 or 0)
 DEBUG=${DEBUG:-1}
-if [ $DEBUG == 0 ]; then
+if [ "$DEBUG" == "0" ]; then
     PETSC_ARCH+=-opt
-    OPTFLAGS="--COPTFLAGS=\"-g -O3 -march=native -mtune=native \" --CXXOPTFLAGS=\"-g -O3 -march=native -mtune=native \" --FOPTFLAGS=\"-g -O3 -march=native -mtune=native \""
 else
-    PETSC_ARCH+=-debug;
+    PETSC_ARCH+=-debug
     DEBUG=1
-    OPTFLAGS=
 fi
 
-#Compilers
+# Compilers
 MYCC=${MYCC:-gcc}
 MYCXX=${MYCXX:-g++}
 MYFC=${MYFC:-gfortran}
@@ -112,32 +101,53 @@ MYFC=${MYFC:-gfortran}
 echo PETSC_DIR=$PETSC_DIR
 echo PETSC_ARCH=$PETSC_ARCH
 
+# Use PRECISION to choose an appropriate BLAS/LAPACK
+if [ "$PRECISION" == "double" ]; then
+    BLAS_LAPACK=" --download-fblaslapack "
+elif [ "$PRECISION" == "single" ]; then
+    BLAS_LAPACK=" --download-fblaslapack "
+elif [ "$PRECISION" == "__float128" ]; then
+    BLAS_LAPACK=" --download-f2cblaslapack "
+fi
+
 # Note that this is a bit non-intuitive using viennacl-dev, as we specify an empty lib location
-if [[ $USE_VIENNACL == 1 ]]; then
+if [ "$USE_VIENNACL" == "1" ] && [ "$SCALARTYPE" != "complex" ] ; then
   VIENNACL_OPTS="--with-opencl "
-  if [[ $USE_CUSTOM_OPENCL_DIR == 1 ]]; then
+  if [ "$USE_CUSTOM_OPENCL_DIR" == "1" ]]; then
     VIENNACL_OPTS+="--with-opencl-dir=$CUSTOM_OPENCL_DIR "
   fi
-  if [[ $VIENNACL_DEV == 1 ]]; then
+  if [ "$VIENNACL_DEV" == "1" ]]; then
     VIENNACL_OPTS+="--with-viennacl=1 --with-viennacl-include=../viennacl-dev --with-viennacl-lib= "
   else
     VIENNACL_OPTS+="--download-viennacl "
   fi
 else
-  VIENNACL_OPTS="--with-viennacl=0"
+  VIENNACL_OPTS=""
 fi
 
-if [[ $USE_C2HTML == 1 ]]; then
+if [ "$USE_C2HTML" == "1" ]; then
   C2HTML_OPTS=""
 else
   C2HTML_OPTS="-with-c2html=0"
 fi
 
-SUITESPARSE_OPTS=""
-if [[ $USE_SUITESPARSE == 1 ]]; then
-if [[ $PRECISION -ne "__float128" ]]; then
+if [ "$USE_SUITESPARSE" == "1" ] && [ "$PRECISION" == "double" ]; then
   SUITESPARSE_OPTS="--download-suitesparse"
+else
+  SUITESPARSE_OPTS=""
 fi
+
+if [ "$EXTRA" == 1 ]; then
+  EXTRA_OPTS=" --download-hdf5 --download-scalapack --download-metis --download-parmetis --download-ptscotch --download-mumps --download-triangle --download-exodusii --download-netcdf --download-chaco"
+  if [ "$PRECISION" == "double" ]; then
+    EXTRA_OPTS+=" --download-sundials "
+  fi
+fi
+
+if [ "$DEBUG" == "0" ]; then
+    OPTFLAGS="--COPTFLAGS=\"-g -O3 -march=native -mtune=native \" --CXXOPTFLAGS=\"-g -O3 -march=native -mtune=native \" --FOPTFLAGS=\"-g -O3 -march=native -mtune=native \""
+else
+    OPTFLAGS=
 fi
 
 # The spaces at the ends of the lines are important.
@@ -157,8 +167,8 @@ $OPTFLAGS \
 $VIENNACL_OPTS \
 $C2HTML_OPTS \
 $SUITESPARSE_OPTS \
-$EXTRAOPTS \
-$EXTRAFLAGS  \
+$EXTRA_OPTS \
+$EXTRAFLAGS \
 "
 echo "Configuring with options:"
 echo $OPTS
