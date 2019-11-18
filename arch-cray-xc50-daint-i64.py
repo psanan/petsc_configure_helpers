9752c4e550908fd2b2175b8cd8f0f0d3ce5911a3
Patrick Sanan <patrick.sanan@gmail.com>
2019-11-18 12:26:18 +0100

    Piz Daint 64-bit PetscInt config

---
+++ arch-cray-xc50-daint-i64.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Piz Daint. requires:
+#  module load PrgEnv-cray
+
+configure_options = [
+  '--with-batch=1',
+  '--with-cc=cc',
+  '--with-cxx=CC',
+  '--with-fc=ftn',
+  '--COPTFLAGS=',
+  '--CXXOPTFLAGS=',
+  '--FOPTFLAGS=',
+  '--with-clib-autodetect=0',
+  '--with-cxxlib-autodetect=0',
+  '--with-fortranlib-autodetect=0',
+  '--with-shared-libraries=0',
+  '--with-debugging=0',
+  '--with-valgrind=0',
+  '--known-mpi-shared-libraries=1',
+  '--with-x=0',
+  '--known-64-bit-blas-indices=0',
+  '--download-suitesparse',
+  '--with-64-bit-indices',
+  'PETSC_ARCH=arch-cray-xc50-daint-i64'
+  ]
+
+if __name__ == '__main__':
+  import sys,os
+  sys.path.insert(0,os.path.abspath('config'))
+  import configure
+  configure.petsc_configure(configure_options)
