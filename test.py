61985d170a224088ccdeeb561acebd0bca27a6e2
Patrick Sanan <patrick.sanan@gmail.com>
2020-06-25 09:09:12 +0200

    Update SciATH test to use line verifier

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ try:
   from sciath.task import Task
   from sciath.job import Job
   from sciath.test import Test
-  from sciath.verifier import ComparisonVerifier
+  from sciath import verifier_line
 except Exception:
   if not sys.exc_info()[-1].tb_next:     # Check that the traceback has depth 1
     traceback.print_exc()
@@ -37,8 +37,8 @@ if not PETSC_SRC_DIR:
 snes_ex_dir = os.path.join(PETSC_SRC_DIR,'src','snes','tutorials')
 
 def main():
-  os.system('make -C ' +  snes_ex_dir + ' clean')
-  os.system('make -C ' +  snes_ex_dir + ' ex19')
+  os.system('make -C %s clean' % snes_ex_dir)
+  os.system('make -C %s ex19' % snes_ex_dir)
 
   Harness([ex19test(1),ex19test(2)]).run_from_args()
 
@@ -46,7 +46,19 @@ def ex19test(ranks):
   test_name = 'ex19_%d' % ranks
   command = [os.path.join(snes_ex_dir,'ex19'),'-da_refine','3','-snes_monitor_short','-pc_type mg','-ksp_type fgmres','-pc_mg_type full']
   test = Test(Job(Task(command), test_name))
-  test.verifier = ComparisonVerifier(test, os.path.join(srcDir, 'ex19.expected'))
+  test.verifier = verifier_line.LineVerifier(test, 'ex19.expected')
+  rtol = 1e-6
+  atol = 1e-10
+  test.verifier.rules.append(verifier_line.key_and_float_rule(
+      'lid velocity', rel_tol=rtol, abs_tol=atol))
+  test.verifier.rules.append(verifier_line.key_and_float_rule(
+      '0 SNES', rel_tol=rtol, abs_tol=atol))
+  test.verifier.rules.append(verifier_line.key_and_float_rule(
+      '1 SNES', rel_tol=rtol, abs_tol=atol))
+  test.verifier.rules.append(verifier_line.key_and_float_rule(
+      '2 SNES', rel_tol=rtol, abs_tol=atol))
+  test.verifier.rules.append(verifier_line.key_and_float_rule(
+      'Number of SNES', rel_tol=rtol, abs_tol=atol))
   return(test)
 
 if __name__ == '__main__':
