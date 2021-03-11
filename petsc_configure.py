45bbee9d97118272aa7116f9da6d204fb02e59e4
Patrick Sanan <patrick.sanan@gmail.com>
2021-03-11 13:24:54 +0100

    Rename configure_options to options

--- petsc_configure.py
+++ petsc_configure.py
@@ -29,9 +29,9 @@ import re
 
 def main():
     """ Main script logic """
-    args, configure_options_in = get_args()
-    configure_options = process_args(configure_options_in, args)
-    petsc_configure(configure_options, args)
+    args, options_in = get_args()
+    options = process_args(options_in, args)
+    petsc_configure(options, args)
 
 
 def get_args():
@@ -67,7 +67,7 @@ def _detect_darwin():
     return sys.platform == 'darwin'
 
 
-def process_args(configure_options_in, args):
+def process_args(options_in, args):
     """ Main logic to create a set of options for PETSc's configure script,
     along with a corresponding PETSC_ARCH string, if required """
 
@@ -86,11 +86,11 @@ def process_args(configure_options_in, args):
     is_darwin = _detect_darwin()
 
     # Initialize options and arch identifiers
-    configure_options = configure_options_in[:]  #copy
+    options = options_in[:]  #copy
     arch_identifiers = initialize_arch_identifiers(args)
 
     # Floating point precision
-    precision = get_option_value(configure_options, "--with-precision")
+    precision = get_option_value(options, "--with-precision")
     if not precision:
         precision = 'double'
     if precision and precision != 'double':
@@ -102,9 +102,9 @@ def process_args(configure_options_in, args):
     # MPI
     # By default, we expect you to have $HOME/code/petsc/arch-mpich-only,
     # which you created with this script.
-    with_mpi = get_option_value(configure_options, "--with-mpi")
-    with_mpi_dir = get_option_value(configure_options, "--with-mpi-dir")
-    download_mpich = get_option_value(configure_options, "--download-mpich")
+    with_mpi = get_option_value(options, "--with-mpi")
+    with_mpi_dir = get_option_value(options, "--with-mpi-dir")
+    download_mpich = get_option_value(options, "--download-mpich")
     if with_mpi != False and not with_mpi_dir and not download_mpich:
         mpich_only_petsc_dir = os.path.join(os.environ['HOME'], 'code', 'petsc')
         mpich_only_dir = os.path.join(mpich_only_petsc_dir, mpich_only_arch)
@@ -113,27 +113,27 @@ def process_args(configure_options_in, args):
             print('Either run this script with --mpich-only from', mpich_only_petsc_dir)
             print('Or specify MPI some other way, e.g. --download-mpich')
             sys.exit(1)
-        configure_options.append('--with-mpi-dir=' + mpich_only_dir)
+        options.append('--with-mpi-dir=' + mpich_only_dir)
 
     # Fortran bindings
-    with_fortran_bindings = get_option_value(configure_options, "--with-fortran-bindings")
+    with_fortran_bindings = get_option_value(options, "--with-fortran-bindings")
     if with_fortran_bindings is None:
-        configure_options.append('--with-fortran-bindings=0')
+        options.append('--with-fortran-bindings=0')
 
     # Integer precision
-    if get_option_value(configure_options, "--with-64-bit-indices"):
+    if get_option_value(options, "--with-64-bit-indices"):
         arch_identifiers.append("int64")
 
     # Scalar type
-    scalartype = get_option_value(configure_options, "--with-scalar-type")
+    scalartype = get_option_value(options, "--with-scalar-type")
     if not scalartype:
         scalartype = 'real'
     if scalartype and scalartype != 'real':
         arch_identifiers.append(scalartype)
-        configure_options.append("--with-scalar-type=%s" % scalartype)
+        options.append("--with-scalar-type=%s" % scalartype)
 
     # C language
-    clanguage = get_option_value(configure_options, "--with-clanguage")
+    clanguage = get_option_value(options, "--with-clanguage")
     if not clanguage:
         clanguage = 'c'
     if clanguage and clanguage != 'c' and clanguage != 'C':
@@ -141,60 +141,60 @@ def process_args(configure_options_in, args):
             arch_identifiers.append('cxx')
 
     # BLAS/LAPACK
-    download_fblaslapack = get_option_value(configure_options,
+    download_fblaslapack = get_option_value(options,
                                             "--download-fblaslapack")
-    download_f2cblaslapack = get_option_value(configure_options,
+    download_f2cblaslapack = get_option_value(options,
                                               "--download-f2cblaslapack")
     if not download_fblaslapack and not download_f2cblaslapack:
         if precision == '__float128':
-            configure_options.append('--download-f2cblaslapack')
+            options.append('--download-f2cblaslapack')
 
     # Extra packages
     if args.extra:
         if args.extra >= 1:
             if scalartype is 'real' and precision is 'double':
-                configure_options.append('--download-suitesparse')
-            configure_options.append('--download-yaml')
+                options.append('--download-suitesparse')
+            options.append('--download-yaml')
         if args.extra >= 2:
-            configure_options.append('--download-scalapack')
-            configure_options.append('--download-metis')
-            download_cmake = get_option_value(configure_options, '--download-cmake')
+            options.append('--download-scalapack')
+            options.append('--download-metis')
+            download_cmake = get_option_value(options, '--download-cmake')
             if download_cmake is None:
-                configure_options.append('--download-cmake')  # for METIS
-            configure_options.append('--download-parmetis')
-            configure_options.append('--download-mumps')
+                options.append('--download-cmake')  # for METIS
+            options.append('--download-parmetis')
+            options.append('--download-mumps')
         if args.extra >= 3:
-            configure_options.append('--download-hdf5')
-            configure_options.append('--download-superlu_dist')
-            with_cuda = get_option_value(configure_options, "--with-cuda")
+            options.append('--download-hdf5')
+            options.append('--download-superlu_dist')
+            with_cuda = get_option_value(options, "--with-cuda")
             if with_cuda:
-                configure_options.append('--with-openmp=1')  # for SuperLU_dist GPU
+                options.append('--with-openmp=1')  # for SuperLU_dist GPU
         if args.extra >= 2:
             arch_identifiers.append('extra')
 
     # Debugging
-    debugging = get_option_value(configure_options, "--with-debugging")
+    debugging = get_option_value(options, "--with-debugging")
     if debugging is None or debugging:
         arch_identifiers.append('debug')
     else:
-        if not get_option_value(configure_options, "--COPTFLAGS"):
-            configure_options.append("--COPTFLAGS=-g -O3 -march=native")
-        if not get_option_value(configure_options, "--CXXOPTFLAGS"):
-            configure_options.append("--CXXOPTFLAGS=-g -O3 -march=native")
-        if not get_option_value(configure_options, "--FOPTFLAGS"):
-            configure_options.append("--FOPTFLAGS=-g -O3 -march=native")
-        if not get_option_value(configure_options, "--CUDAOPTFLAGS"):
-            configure_options.append("--CUDAOPTFLAGS=-O3")
+        if not get_option_value(options, "--COPTFLAGS"):
+            options.append("--COPTFLAGS=-g -O3 -march=native")
+        if not get_option_value(options, "--CXXOPTFLAGS"):
+            options.append("--CXXOPTFLAGS=-g -O3 -march=native")
+        if not get_option_value(options, "--FOPTFLAGS"):
+            options.append("--FOPTFLAGS=-g -O3 -march=native")
+        if not get_option_value(options, "--CUDAOPTFLAGS"):
+            options.append("--CUDAOPTFLAGS=-O3")
         arch_identifiers.append('opt')
 
     # C2HTML (for building docs locally)
-    with_c2html = get_option_value(configure_options, '--with-c2html')
-    download_c2html = get_option_value(configure_options, '--download-c2html')
+    with_c2html = get_option_value(options, '--with-c2html')
+    download_c2html = get_option_value(options, '--download-c2html')
     if not with_c2html != False and download_c2html != False:
-        configure_options.append("--download-c2html")
+        options.append("--download-c2html")
 
     # Prefix
-    prefix = get_option_value(configure_options, "--prefix")
+    prefix = get_option_value(options, "--prefix")
     if prefix:
         arch_identifiers.append('prefix')
 
@@ -203,24 +203,24 @@ def process_args(configure_options_in, args):
     if args.prefix_auto:
         if prefix:
             raise RuntimeError('Cannot use both --prefix and --prefix-auto')
-        configure_options.append(
+        options.append(
             '--prefix=' +
             os.path.join(os.getcwd(), '-'.join(arch_identifiers) + '-install'))
         arch_identifiers.append('prefix')
 
     # Add PETSC_ARCH
-    configure_options.append('PETSC_ARCH=' + '-'.join(arch_identifiers))
+    options.append('PETSC_ARCH=' + '-'.join(arch_identifiers))
 
     # Use the current directory as PETSC_DIR
-    configure_options.append('PETSC_DIR=' + os.getcwd())
+    options.append('PETSC_DIR=' + os.getcwd())
 
-    return configure_options
+    return options
 
 
-def get_option_value(configure_options, key):
+def get_option_value(options, key):
     """ Get the value of a configure option """
     regexp = re.compile(key)
-    matches = list(filter(regexp.match, configure_options))
+    matches = list(filter(regexp.match, options))
     # FIXME this doesn't work with e.g. --with-cuda=1 --with-cuda-dir=foo:
     if len(matches) > 1:
         raise RuntimeError('More than one match for option', key)
@@ -256,30 +256,30 @@ def initialize_arch_identifiers(args):
 
 def options_for_mpich_only(mpich_only_arch):
     """ Return a custom set of arguments to simply download and build MPICH """
-    configure_options = []
-    configure_options.append('--download-mpich')
+    options = []
+    options.append('--download-mpich')
     if _detect_darwin():
-        configure_options.append('--with-cc=ccache /usr/bin/gcc')
-        configure_options.append('--with-cxx=ccache /usr/bin/g++')
+        options.append('--with-cc=ccache /usr/bin/gcc')
+        options.append('--with-cxx=ccache /usr/bin/g++')
     else:
-        configure_options.append('--with-cc=ccache gcc')
-        configure_options.append('--with-cxx=ccache g++')
-    configure_options.append('--with-fc=ccache gfortran')
-    configure_options.append('--with-x=0')
-    configure_options.append('--with-debugging=0')
-    configure_options.append("--COPTFLAGS=-g -O3")
-    configure_options.append("--CXXOPTFLAGS=-g -O3")
-    configure_options.append("--FOPTFLAGS=-g -O3")
-    configure_options.append('PETSC_ARCH=' + mpich_only_arch)
-    configure_options.append('PETSC_DIR=' + os.getcwd())
-    return configure_options
+        options.append('--with-cc=ccache gcc')
+        options.append('--with-cxx=ccache g++')
+    options.append('--with-fc=ccache gfortran')
+    options.append('--with-x=0')
+    options.append('--with-debugging=0')
+    options.append("--COPTFLAGS=-g -O3")
+    options.append("--CXXOPTFLAGS=-g -O3")
+    options.append("--FOPTFLAGS=-g -O3")
+    options.append('PETSC_ARCH=' + mpich_only_arch)
+    options.append('PETSC_DIR=' + os.getcwd())
+    return options
 
 
-def petsc_configure(configure_options, args):
+def petsc_configure(options, args):
     """ Standard PETSc configuration script logic (from config/examples) """
     if args.dryrun:
         print("Dry Run. Would configure with these options:")
-        print("\n".join(configure_options))
+        print("\n".join(options))
     else:
         sys.path.insert(0, os.path.abspath('config'))
         try:
@@ -290,8 +290,8 @@ def petsc_configure(configure_options, args):
             )
             sys.exit(1)
         print('Configuring with these options:')
-        print("\n".join(configure_options))
-        configure.petsc_configure(configure_options)
+        print("\n".join(options))
+        configure.petsc_configure(options)
 
 
 if __name__ == '__main__':
